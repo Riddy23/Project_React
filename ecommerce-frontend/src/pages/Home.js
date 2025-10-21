7692a4a320317173b4f1df9424ef721e5f762fed
Riddy23 <redemptionsuwali@gmail.com>
2025-10-21 10:32:50 +0200

    Add Home component for e-commerce frontend

---
+++ ecommerce-frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to E-commerce Frontend</h1>
+      <p>Use the navigation bar to browse products, tasks, and users.</p>
+    </div>
+  );
+}
+
+export default Home;
